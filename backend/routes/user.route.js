5ed0c12189fa95e10965bd61f3bd79f9a8a0130c
rares22iunie <raresdoroftei@yahoo.com>
2022-12-15 01:18:40 +0200

    final commit

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,51 +1,72 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const validator = require("email-validator");
 
-// /api/user
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    req.body.username &&
+    req.body.email &&
+    req.body.password &&
+    req.body.password.length > 5 &&
+    validator.validate(req.body.email) == true
+  ) {
     try {
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        });
+      bcrypt
+        .hash(password, 2)
+        .then(async (hash) => {
+          const user = new User({
+            username: username,
+            email: email,
+            password: hash,
+          });
 
-        console.log(user);
+          const duplicateUser = await User.findOne({ email: user.email });
 
-        const duplicateUser = await User.findOne({email: user.email});
-
-        console.log(duplicateUser);
-
-        if (duplicateUser) {
+          if (duplicateUser) {
             return res.send({
-                success: false,
-                message: "User with that email already exists!"
-            })
-        }
+              success: false,
+              message: "User with that email already exists!",
+            });
+          }
 
-        const duplicateUser2 = await User.findOne({username: user.username});
+          const duplicateUser2 = await User.findOne({
+            username: user.username,
+          });
 
-        if (duplicateUser2) {
+          if (duplicateUser2) {
             return res.send({
-                success: false,
-                message: "User with that username already exists!"
-            })
-        }
+              success: false,
+              message: "User with that username already exists!",
+            });
+          }
 
-        // User.find() -> returneaza TOTI userii
-        // User.findByID(_id) -> returneaza un user cu id-ul _id, daca exista
+          // User.find() -> returneaza TOTI userii
+          // User.findByID(_id) -> returneaza un user cu id-ul _id, daca exista
 
-        user.save();
+          user.save();
 
-        return res.send({
-            success: true
+          return res.send({
+            success: true,
+          });
         })
+        .catch((error) =>
+          res.status(400).json({
+            message: "Password not hashed",
+            error: error.message,
+          })
+        );
     } catch (e) {
-        return res.send({
-            success: false,
-            message: "Error: " + e.message
-        })
+      return res.status(400).send({
+        message: "Error: " + e.message,
+      });
     }
-})
+  } else {
+    return res.status(400).send({
+      message: "Data incomplete or password too small or bad email",
+    });
+  }
+});
 
 module.exports = router;
